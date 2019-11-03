1e7da942f9c744785303be22ef5c9e79cdf5d48a
joeflow <thejoeflow@gmail.com>
2019-11-03 11:28:44 -0500

    UI changes

--- src/__init__.py
+++ src/__init__.py
@@ -6,4 +6,4 @@ webapp = Flask(__name__)
 webapp.secret_key = config.app_config['secret_key']
 webapp.config['MAX_CONTENT_LENGTH'] = config.app_config['max_file_size_MB'] * 1024 * 1024
 
-from src import main, ec2_examples, s3_examples
+from src import main
